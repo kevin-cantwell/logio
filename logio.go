2116c5c5765bba51f141536066a9e0dc2c6f8376
Kevin Cantwell <kevin.cantwell@gmail.com>
2016-11-28 14:37:24 -0500

    redial bad connections

--- logio.go
+++ logio.go
@@ -1,29 +1,86 @@
 package logio
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
+var Debug = false
+
+func debug(a ...interface{}) {
+	if Debug {
+		fmt.Println(a...)
+	}
+}
+
 func init() {
-	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:7514")
-	if err != nil {
-		panic(err)
+	addr := os.Getenv("LOGIO_SERVER")
+	if addr == "" {
+		return
 	}
-
-	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-	if err != nil {
-		panic(err)
+	serverConn := &logioConn{
+		addr:  addr,
+		logch: make(chan []byte),
 	}
+	go serverConn.sendAll()
 
-	conn, err := net.DialUDP("udp", localAddr, serverAddr)
-	if err != nil {
-		panic(err)
-	}
-
-	multi := io.MultiWriter(os.Stderr, conn)
-
+	multi := io.MultiWriter(os.Stderr, serverConn)
 	log.SetOutput(multi)
+}
+
+type logioConn struct {
+	addr string
+	conn net.Conn
+	mu   sync.RWMutex
+
+	logch chan []byte
+}
+
+func (reconn *logioConn) Write(logmsg []byte) (int, error) {
+	select {
+	case reconn.logch <- logmsg:
+		return len(logmsg), nil
+	default:
+		return 0, fmt.Errorf("error: logio buffer too full")
+	}
+}
+
+func (reconn *logioConn) sendAll() {
+	if err := reconn.dial(); err != nil {
+		debug("logio:", "error dialing udp conn:", err)
+		return
+	}
+	for logmsg := range reconn.logch {
+		if err := reconn.robustlySend(logmsg); err != nil {
+			debug("logio:", err)
+		}
+	}
+}
+
+func (reconn *logioConn) robustlySend(logmsg []byte) error {
+	_, err := reconn.conn.Write(logmsg)
+	switch err.(type) {
+	case *net.OpError:
+		if err := reconn.dial(); err != nil {
+			return err
+		}
+		return reconn.robustlySend(logmsg)
+	}
+	return err
+}
+
+func (reconn *logioConn) dial() error {
+	reconn.mu.Lock()
+	defer reconn.mu.Unlock()
+
+	conn, err := net.Dial("udp", reconn.addr)
+	if err != nil {
+		return err
+	}
+	reconn.conn = conn
+	return nil
 }
